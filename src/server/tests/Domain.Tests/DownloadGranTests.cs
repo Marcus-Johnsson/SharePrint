c89827d7240b2ee44739308e0356c20e1a518f26
Marcus Johnsson <marcus-johnsson@hotmail.se>
2026-05-19 09:14:44 +0200

    added two test for download limit testing

--- src/server/tests/Domain.Tests/DownloadGranTests.cs
+++ src/server/tests/Domain.Tests/DownloadGranTests.cs
@@ -2,5 +2,19 @@ namespace SharePrint.Domain.Tests;
 
 public class DownloadGranTests
 {
-    
+    [Fact]
+    public void IssueDownload_decrements_and_stamps()
+    {
+        var g = new DownloadGrant { OrderItemId = Guid.NewGuid(), DownloadRemaining = 5 };
+        g.IssueDownload();
+        Assert.Equal(4, g.DownloadRemaining);
+        Assert.NotNull(g.LastDownloadedAt);
+    }
+
+    [Fact]
+    public void IssueDownload_throws_when_exausted()
+    {
+        var g = new DownloadGrant { OrderItemId = Guid.NewGuid(), DownloadRemaining = 0 };
+        Assert.Throws<InvalidOperationException>(() => g.IssueDownload());
+    }
 }
